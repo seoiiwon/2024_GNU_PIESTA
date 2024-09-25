b07f00ed07fbb2877e09016989caebae390791da
Seo Jiwon <wldnjstj99@gmail.com>
2024-09-26 04:15:18 +0900

    home page3 editing..

--- static/js/main/home.js
+++ static/js/main/home.js
@@ -1,8 +1,9 @@
-// 페이지 스크롤 이벤트
+// 스크롤 페이지 전환
 document.addEventListener("DOMContentLoaded", function() {
     let currentSection = 0;
     const sections = document.querySelectorAll('div');
     const totalSections = sections.length;
+    const logoImg = document.getElementById('logoImg');
 
     function scrollToSection(sectionIndex) {
         window.scrollTo({
@@ -11,21 +12,41 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // 첫 번째 페이지 로고 숨기기
+    function handleLogoPosition() {
+        if (currentSection === 1 || currentSection === 2) {
+            logoImg.style.position = 'fixed';
+            logoImg.style.visibility = 'visible';
+            logoImg.style.opacity = '1';
+        } else if (currentSection === 0) {
+            logoImg.style.opacity = '0';
+            setTimeout(() => {
+                logoImg.style.visibility = 'hidden';
+            }, 300);
+        }
+    }
+
+    // 스크롤 이벤트
     window.addEventListener('wheel', function(event) {
         if (event.deltaY > 0) {
             if (currentSection < totalSections - 1) {
                 currentSection++;
                 scrollToSection(currentSection);
+                handleLogoPosition();
             }
         } else {
             if (currentSection > 0) {
                 currentSection--;
                 scrollToSection(currentSection);
+                handleLogoPosition();
             }
         }
     });
 
     window.addEventListener('resize', function() {
         scrollToSection(currentSection);
+        handleLogoPosition();
     });
+
+    handleLogoPosition();
 });
