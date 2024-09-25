ecf88b5c415b9c7988df13dadcf60a66a7823c74
Seo Jiwon <wldnjstj99@gmail.com>
2024-09-26 01:44:03 +0900

    db reset

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./2024_GNU_FIESTA.db"
+DATABASE_URL = "sqlite:///./2024_GNU_PIESTA.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
