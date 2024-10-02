9b1d42cdc6b523e72da80b5e02e4ab324acd2c6c
unsag <unsagaxma@naver.com>
2024-10-02 13:26:32 +0900

    세팅

--- api/booth/booth_list_router.py
+++ api/booth/booth_list_router.py
@@ -4,3 +4,9 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter(tags=["BOOTH"])
 
+templates = Jinja2Templates(directory="./templates/booth")
+
+
+@router.get("/GNU_PIESTA/booth_list", response_class=HTMLResponse)
+async def getHome(request: Request):
+    return templates.TemplateResponse(name="booth_list.html", request=request)
