3dc7d3daacc07388224c97665af96c4d32047e86
Seo Jiwon <wldnjstj99@gmail.com>
2024-10-09 10:39:10 +0900

    update port

--- main.py
+++ main.py
@@ -65,11 +65,9 @@ app.include_router(notice.router)
 #     return FileResponse("favicon.ico")
 
 
-
-
 HOST = "0.0.0.0"
 # HOST = "172.30.1.11"
-PORT = 80
+PORT = 8080
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
