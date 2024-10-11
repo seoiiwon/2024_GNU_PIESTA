0e60350d9b74d79d6eef2bceda5b8ddc2a70be2a
Seo Jiwon <wldnjstj99@gmail.com>
2024-10-11 21:50:22 +0900

    update bus info

--- static/js/main/modal.js
+++ static/js/main/modal.js
@@ -10,14 +10,13 @@ function showNoticeModal(noticeCategory, noticeTitle, noticeBody) {
     noticeModalTitle.textContent = noticeTitle;
     noticeModalBody.textContent = noticeBody;
 
-    noticeModal.style.display = 'block';
+    document.getElementById('home_info').classList.add('blur-background');
+    noticeModal.style.display = 'flex';
   } else {
     console.log('404 NOT FOUND');
     
   }
 }
-
-
 
 let busInfo_chilam = {};
 let busInfo_tongyeong = {};
@@ -32,8 +31,6 @@ fetch(jsonFilePath)
     })
     .then(data => {
        busInfo_chilam = data["칠암"];
-      //  busInfo_tongyeong = data["통영"];
-      //  busInfo_changwon = data["창원"];
       busInfo_all = data;
        console.log(busInfo_tongyeong);
        
@@ -41,40 +38,41 @@ fetch(jsonFilePath)
     .catch(error => console.error("Error fetching JSON:", error));
 
 
-// 모달 열기
+
 function showBusModalChilam(busNumber) {
-  const bus = busInfo_chilam[busNumber];
+    const bus = busInfo_chilam[busNumber];
 
-  if (bus) {
-      const startPoint = bus["startPoint"];  
-      const endPoint = bus["endPoint"];      
+    if (bus) {
+        const startPoint = bus["startPoint"];  
+        const endPoint = bus["endPoint"];      
 
-      const sp = startPoint["sp"];                         
-      const firstBusFromStartPoint = startPoint["firstBus"]; 
-      const lastBusFromStartPoint = startPoint["lastBus"];   
-      const termFromStartPoint = startPoint["term"];         
+        const sp = startPoint["sp"];                         
+        const firstBusFromStartPoint = startPoint["firstBus"]; 
+        const lastBusFromStartPoint = startPoint["lastBus"];   
+        const termFromStartPoint = startPoint["term"];         
 
-      const lp = endPoint["lp"];                           
-      const firstBusFromEndPoint = endPoint["firstBus"];   
-      const lastBusFromEndPoint = endPoint["lastBus"];     
-      const termFromEndPoint = endPoint["term"];           
+        const lp = endPoint["lp"];                           
+        const firstBusFromEndPoint = endPoint["firstBus"];   
+        const lastBusFromEndPoint = endPoint["lastBus"];     
+        const termFromEndPoint = endPoint["term"];           
 
-      // 각 요소에 데이터를 할당
-      document.getElementById('busNumber').textContent = busNumber; 
-      document.getElementById('busChannel').textContent = `${sp} - ${lp}`; 
-      document.getElementById('firstTimeFromStartPoint').textContent = firstBusFromStartPoint; 
-      document.getElementById('lastTimeFromStartPoint').textContent = lastBusFromStartPoint;
-      document.getElementById('termsFromStartPoint').textContent = termFromStartPoint;
-      
-      document.getElementById('firstTimeFromEndPoint').textContent = firstBusFromEndPoint;
-      document.getElementById('lastTimeFromEndPoint').textContent = lastBusFromEndPoint;
-      document.getElementById('termsFromEndPoint').textContent = termFromEndPoint;
+        document.getElementById('busNumber').textContent = busNumber; 
+        document.getElementById('busChannel').textContent = `${sp} - ${lp}`; 
+        document.getElementById('firstTimeFromStartPoint').textContent = firstBusFromStartPoint; 
+        document.getElementById('lastTimeFromStartPoint').textContent = lastBusFromStartPoint;
+        document.getElementById('termsFromStartPoint').textContent = termFromStartPoint;
+        
+        document.getElementById('firstTimeFromEndPoint').textContent = firstBusFromEndPoint;
+        document.getElementById('lastTimeFromEndPoint').textContent = lastBusFromEndPoint;
+        document.getElementById('termsFromEndPoint').textContent = termFromEndPoint;
 
-      document.getElementById("Modal1").style.display = "block";
-  } else {
-      alert('해당 버스 정보를 찾을 수 없습니다.'); 
-  }
+        document.getElementById('home_info').classList.add('blur-background');
+        document.getElementById("busModal1").style.display = "flex";
+    } else {
+        alert('해당 버스 정보를 찾을 수 없습니다.'); 
+    }
 }
+
 
 function showBusModalTYCW(time, location, busFrom) {
   const bus = busInfo_all[location][busFrom][time];
@@ -100,35 +98,36 @@ function showBusModalTYCW(time, location, busFrom) {
     const type = bus["유형"];
     const path = bus["경유"];
 
-    document.getElementById("busChannel").textContent = channelText;
+    document.getElementById("busChannel-TYCW").textContent = channelText;
+    
     document.getElementById("busTime").textContent = time;
     document.getElementById("busExpense").textContent = expense;
     document.getElementById("busType").textContent = type;
     document.getElementById("busPath").textContent = path;
     document.getElementById("busCompany").textContent = company;
 
-    document.getElementById("Modal2").style.display = "block";
+    document.getElementById('home_info').classList.add('blur-background');
+    document.getElementById("busModal2").classList.add('modal-active');
+
+    document.getElementById("busModal2").style.display = "flex";
   } else {
     alert('해당 버스 정보를 찾을 수 없습니다.')
   }  
 }
 
-// 모달 바깥을 클릭하면 닫기
-window.addEventListener('click', handleModalClose);
-window.addEventListener('touchstart', handleModalClose); 
 
-function handleModalClose(event) {
-    const modal1 = document.getElementById('Modal1');
-    const modal2 = document.getElementById('Modal2');
-    const noticeModal = document.getElementById('noticeModal');
-
-    if (!modal1.contains(event.target) && !modal2.contains(event.target) && !noticeModal.contains(event.target)) {
-        modal1.style.display = "none";
-        modal2.style.display = "none";
-        noticeModal.style.display = "none";
-    }
+// 모달 닫기
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.getElementById('home_info').classList.remove('blur-background');
 }
+  
+window.onclick = function(event) {
+const noticeModal = document.getElementById('noticeModal');
+const busModal1 = document.getElementById('busModal1');
+const busModal2 = document.getElementById('busModal2');
 
-document.addEventListener('touchmove', function(event) {
-    event.preventDefault(); // 터치 이동 시 기본 동작 방지 
-}, { passive: false }); 
+if (event.target == noticeModal || event.target == busModal1 || event.target == busModal2) {
+    closeModal(event.target.id);
+}
+}
